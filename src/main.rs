641804007fa6207d1e29a468222c403b2dde3628
Oliver <oliver.e.v@gmail.com>
2025-02-16 12:39:17 +1100

    attempt mutex

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,19 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-fn audio_tick_with_reload() {
+fn audio_tick_with_reload(mutex: Arc<Mutex<HotReloadState<'_>>>) {
     loop {
+      let lock = mutex.lock().unwrap();
+      let blah = &lock.audio_tick;
+      blah.as_ref().unwrap()();
+      // match blah {
+      //   Some(audio_tick) => {
+      //     audio_tick();
+      //   }
+      //   None => {
+      //     println!("oops, no audio tick");
+      //   }
+      // }
       // spam load the .so over and over.
       // how to improve?
       let audio_lib_path = std::env::current_dir().unwrap().join("audio").join("target").join("debug").join("libaudio.so");
@@ -17,8 +28,11 @@ fn audio_tick_with_reload() {
     }
 }
 
-fn input_tick() {
+fn input_tick(mutex: Arc<Mutex<HotReloadState<'_>>>) {
     loop {
+        let lock = mutex.lock().unwrap();
+        let blah = &lock.audio_tick;
+        blah.as_ref().unwrap()();
         println!("Input tick at {:?}", std::time::SystemTime::now());
         thread::sleep(Duration::from_millis(0)); // Simulate work
     }
@@ -55,10 +69,24 @@ fn main() {
     };
 
     let hot_reload_mutex = std::sync::Arc::new(std::sync::Mutex::new(hot_reload_state));
+    let hot_reload_mutex_audio_thread = hot_reload_mutex.clone();
+    let hot_reload_mutex_input_thread = hot_reload_mutex.clone();
+
+    let fns = vec![
+      audio_tick_with_reload, 
+      input_tick,
+    ];
+
+    let handles_2 = fns.into_iter().map(|f| {
+      let mutex = hot_reload_mutex.clone();
+      thread::spawn(move || {
+        f(mutex);
+      })
+    });
 
     let handles = vec![
-        thread::spawn(audio_tick_with_reload),
-        // thread::spawn(input_tick),
+        thread::spawn(move || { audio_tick_with_reload(hot_reload_mutex_audio_thread); }),
+        thread::spawn(move || { input_tick(hot_reload_mutex_input_thread); }),
         // thread::spawn(game_tick),
         // thread::spawn(render_tick),
         // thread::spawn(ai_tick),
